f17c604e93e8483bc01246ea1c8da29838c6fd0c
Nikita Ryasn <nikto1252005228@gmail.com>
2025-12-28 00:24:47 +0300

    Fixed index.html x2

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
+
 
 
 const db = new Pool({
